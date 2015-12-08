8e3339bcea3d6d68306d3f4ea86c879cf05a58e8
Askarbek Karasaev <askar.karasaev@gmail.com>
2015-12-08 15:51:49 +0900

    index.php added

--- index.php
+++ index.php
@@ -1,1 +1,35 @@
-// coming soon
+<?php
+
+ $db = mysql_connect("localhost","root","root"); 
+ if (!$db) {
+ die("Database connection failed miserably: " . mysql_error());
+ }
+
+ $db_select = mysql_select_db("catalog",$db);
+ if (!$db_select) {
+ die("Database selection also failed miserably: " . mysql_error());
+ }
+?>
+<html>
+ <head>
+ <title>Catalog</title>
+ </head>
+ <body>
+ <?php
+
+$result = mysql_query("SELECT * FROM products", $db);
+ if (!$result) {
+ die("Database query failed: " . mysql_error());
+ }
+
+ while ($row = mysql_fetch_array($result)) {
+ echo $row[1]." ".$row[2]."<br />";
+ }
+?>
+ </body>
+</html>
+
+<?php
+//Step5
+ mysql_close($db);
+?>
